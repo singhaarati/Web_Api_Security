594d9cd56b7009adda183e61ebc4c208f228200b
singhaarati <aarati5737@gmail.com>
2023-11-03 10:11:05 +0545

    user and admin verify done

---
+++ middlewares/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken')
+
+const verifyUser = (req, res, next) => {
+    let token = req.headers.authorization
+    if (!token) return res.status(401).json({ error: 'auth token not present' })
+    token = token.split(' ')[1]
+
+    jwt.verify(token, process.env.SECRET, (err, payload) => {
+        if (err) return res.status(401).json({ error: err.message })
+        req.user = payload
+        console.log(req.user)
+
+        next();
+    })
+    // next()
+}
+
+const verifyAdmin = (req, res, next) => {
+    if (req.user.role !== 'admin') {
+        return res.status(403).json({ error: 'you are not admin!' })
+    } else if (req.user.role === 'admin') {
+        next()
+    }
+}
+module.exports = { verifyUser, verifyAdmin }

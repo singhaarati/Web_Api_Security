36d7068748815b90585fa558722b8c0c8c4f95ba
singhaarati <aarati5737@gmail.com>
2023-11-05 17:37:30 +0545

    user review done

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -42,7 +42,58 @@ const deleteAllReviews = (req, res, next) => {
         }).catch(next)
 }
 
+// Get a review by ID
 
+const getReviewById = (req, res, next) => {
+    Destination.findById(req.params.Destination_id)
+        .then((destination) => {
+            if (!destination) return res.status(404).json({ error: 'destination not found' })
+            const review = destination.reviews.id(req.params.review_id)
+            if (!review) return res.status(404).json({ error: 'review not found' })
+            res.json(review)
+        }).catch(next)
+}
+
+
+//update a review
+
+const updateReviewById = (req, res, next) => {
+    Destination.findById(req.params.Destination_id)
+        .then((destination) => {
+            if (!destination) return res.status(404).json({ error: 'destination not found' })
+            const reviewIndex = destination.reviews.findIndex(
+                (review) => review._id.toString() === req.params.review_id
+            );
+            if (reviewIndex === -1) {
+                return res.status(404).json({ error: 'Review not found' });
+            }
+            destination.reviews[reviewIndex].text = req.body.text;
+            destination.save()
+                .then((destination) => res.json(destination.reviews[reviewIndex]))
+                .catch(next);
+        })
+        .catch(next);
+};
+
+//delete a review
+
+const deleteReviewById = (req, res, next) => {
+    Destination.findById(req.params.Destination_id)
+        .then(destination => {
+            if (!destination) return res.status(404).json({ error: 'destination not found' })
+            const reviewIndex = destination.reviews.findIndex(
+                (review) => review._id.toString() === req.params.review_id
+            );
+            if (reviewIndex === -1) {
+                return res.status(404).json({ error: 'Review not found' });
+            }
+            destination.reviews.splice(reviewIndex, 1);
+            destination.save()
+                .then(() => res.json({ message: 'Review deleted successfully' }))
+                .catch(next);
+        })
+        .catch(next);
+};
 
 // Export the controller functions
 module.exports = {
